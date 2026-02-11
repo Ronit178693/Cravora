4b8ea4978592a481d96bdaafe2256ac97230c996
Ronit178693 <ronitagrawal9921@gmail.com>
2026-02-11 23:37:42 +0530

    Designed User Model

---
+++ Server/src/Models/User.js
@@ -0,0 +1,67 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+
+
+const UserSchema = new mongoose.Schema({
+
+    // User Details
+    name: {
+    type: String,
+    required: [true, 'Please provide a name'],
+    trim: true,
+    minlength: [2, 'Name must be at least 2 characters long']
+  },
+  email: {
+    type: String,
+    required: [true, 'Please provide an email'],
+    unique: true, 
+    lowercase: true,
+    match: [/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/, 'Please provide a valid email']
+  },
+  phoneNumber: {
+    type: String,
+    required: [true, 'Please provide a phone number'],
+    match: [/^[0-9]{10}$/, 'Please provide a valid 10-digit phone number']
+  },
+  password: {
+    type: String,
+    required: [true, 'Please provide a password'],
+    minlength: [6, 'Password must be at least 6 characters long'],
+    select: false
+  },
+  role: {
+    type: String,
+    enum: ['Student', 'Outlet', 'DeliveryPartner', 'Admin'],
+    default: 'Student'
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+    // Enhanced tracking fields
+  deliveryStats: {
+    deliveriesCompleted: { type: Number, default: 0 },
+    // ratings: [{
+    //   order: { type: mongoose.Schema.Types.ObjectId, ref: 'Order' },
+    //   rating: { type: Number, min: 1, max: 5 },
+    //   feedback: String
+    // }]
+  },
+  orderHistory: [
+    {
+      order: { type: mongoose.Schema.Types.ObjectId, ref: 'Order' },
+      orderedAt: { type: Date, default: Date.now }
+    }
+  ],
+  orderStatus: [{
+    order: { type: mongoose.Schema.Types.ObjectId, ref: 'Order' },
+    status: { type: String, enum: ['Pending', 'Accepted', 'Completed', 'Cancelled'], default: 'Pending' },
+    deliveredAt: { type: Date },
+    // rating: { type: Number, min: 1, max: 5 }
+  }
+  ],
+  lastDeliveryAt: { type: Date }
+}, { timestamps: true })
+
+// Hashing password before saving the password 
+
