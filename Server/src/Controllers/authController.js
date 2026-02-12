e4635d1e3a91e752ef9c2196b68196b33aa1ce90
Ronit178693 <ronitagrawal9921@gmail.com>
2026-02-12 09:20:33 +0530

    Completed userController

---
+++ Server/src/Controllers/authController.js
@@ -0,0 +1,20 @@
+import User from "../Models/User.js";
+import jwt from "jsonwebtoken";
+
+
+export const register = async (req, res) => {
+    const { name, email, password } = req.body;
+    try {
+        const user = await User.findOne({ email });
+        if (user) {
+            return res.status(400).json({ message: 'User already exists' });
+        }
+        const newUser = new User({ name, email, password });
+        await newUser.save();
+        res.status(201).json({ message: 'User registered successfully' });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+}
+
+
--- Server/src/Controllers/userController.js
+++ Server/src/Controllers/userController.js
@@ -1,8 +1,12 @@
 import User from "../Models/User.js";
 
+
+// Get user function to get user info 
 export const getUser = async (req, res) => {
+
+    const UserID = req.user.id;
     try {
-        const user = await User.findById(req.params.id);
+        const user = await User.findById(UserID);
         if (!user) {
             return res.status(404).json({ message: 'User not found' });
         }
