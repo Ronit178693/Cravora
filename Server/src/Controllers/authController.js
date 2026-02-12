f31fffe4fcb62fcfbb100756848db95d731d611d
Ronit178693 <ronitagrawal9921@gmail.com>
2026-02-12 14:27:40 +0530

    Written Login API Endpoint

--- Server/src/Controllers/authController.js
+++ Server/src/Controllers/authController.js
@@ -1,5 +1,5 @@
 import User from "../Models/User.js";
-import { hashPassword } from "../Models/User.js";
+import { hashPassword, comparePassword} from "../Models/User.js";
 import jwt from "jsonwebtoken";
 
 export const Register = async (req, res) => {
@@ -41,5 +41,46 @@ export const Register = async (req, res) => {
         return res.status(500).json({success: false, message: error.message});
     }
 }
+
+export const Login = async (req, res) => {
+    const {email, password} = req.body;
+    try{
+        // Role is not necessary as it is already stored in the database
+        if(!email || !password){
+            return res.status(400).json({success: false, message: "All fields are required"});
+        }
+        // Check if the email and password exists in the database 
+        const user = await User.findOne({email}).select("+password");
+        // Checking if the user does not exists 
+        if(!user){
+            return res.status(400).json({success: false, message: "User not found"});
+        }
+        // Checking if the password is correct
+        if(!await comparePassword(password, user.password)){
+            return res.status(400).json({success: false, message: "Invalid password"});
+        }
+        // Creating a token and sending info like userID and role 
+        const token = jwt.sign({
+            id: user._id,
+            role: user.role
+        }, process.env.JWT_SECRET, {
+            expiresIn: "30d"
+        })
+        // Sending token to the client as cookie
+        // Evey time any request is made after logging in or signing in the cookie is automatically sent from the client to the server
+        res.cookie("token", token, {
+            httpOnly: true, 
+            secure: process.env.NODE_ENV == "production", // The cookie is only sent over HTTPS on local hoast it fails sending cookie
+            sameSite: "strict",
+            maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days in milliseconds
+        })
+
+        return res.status(200).json({success: true, message: "User logged in successfully"});
+    }
+    catch(error){
+        return res.status(500).json({success: false, message: error.message});
+    }
+}
+
 
 
--- Server/src/server.js
+++ Server/src/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
 
 dotenv.config();
@@ -7,6 +8,7 @@ dotenv.config();
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
 
 
 app.listen(process.env.PORT || 5000, () => {
