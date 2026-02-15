9a96a9e21732fcf4dd78bb67df9ddb848044fc11
Ronit178693 <ronitagrawal9921@gmail.com>
2026-02-15 15:22:03 +0530

    Added dashboard routes

--- Server/src/Routes/dashboardRoutes.js
+++ Server/src/Routes/dashboardRoutes.js
@@ -1,1 +1,14 @@
+import express from "express";
+import { getDashboard, getProfile, getRunnerDashboard, getRunnerStats } from "../Controllers/dashboardController.js";
+import { protect } from "../Middlewares/authMiddleware.js";
+import { authorize } from "../Middlewares/roleMiddleware.js";
 
+const router = express.Router();
+
+// All dashboard routes require authentication
+router.get("/", protect, getDashboard);
+router.get("/profile", protect, getProfile);
+router.get("/runner", protect, authorize("Student", "DeliveryPartner"), getRunnerDashboard);
+router.get("/runner/stats", protect, authorize("Student", "DeliveryPartner"), getRunnerStats);
+
+export default router;
