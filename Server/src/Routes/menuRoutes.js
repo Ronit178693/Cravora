55e9372ed36e5b894f2ae5527306ed869901b3b1
Ronit178693 <ronitagrawal9921@gmail.com>
2026-02-15 15:23:06 +0530

    Added menu routes

--- Server/src/Routes/menuRoutes.js
+++ Server/src/Routes/menuRoutes.js
@@ -1,1 +1,13 @@
+import express from "express";
+import { addMenuItem, updateMenuItem, deleteMenuItem } from "../Controllers/menuController.js";
+import { protect } from "../Middlewares/authMiddleware.js";
+import { authorize } from "../Middlewares/roleMiddleware.js";
 
+const router = express.Router();
+
+// All menu routes require auth + Outlet role
+router.post("/:id", protect, authorize("Outlet"), addMenuItem);
+router.put("/:id/item/:itemId", protect, authorize("Outlet"), updateMenuItem);
+router.delete("/:id/item/:itemId", protect, authorize("Outlet"), deleteMenuItem);
+
+export default router;
