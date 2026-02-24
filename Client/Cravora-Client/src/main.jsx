b451328aba73f4eea5b5d428f35c72e4c2a6091b
Ronit178693 <ronitagrawal9921@gmail.com>
2026-02-24 18:19:05 +0530

    Wrapped the app with CartProvider

--- Client/Cravora-Client/src/main.jsx
+++ Client/Cravora-Client/src/main.jsx
@@ -1,10 +1,14 @@
 import { createRoot } from 'react-dom/client'
 import { AuthProvider } from './context/AuthContext'
+import { CartProvider } from './context/CartContext'
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <AuthProvider>
-    <App />
+    <CartProvider>
+      <App />
+    </CartProvider>
   </AuthProvider>
 )
+
