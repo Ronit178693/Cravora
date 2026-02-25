46863f6a0d030595fdc48f8aaad6265b80e2e0d4
Ronit178693 <ronitagrawal9921@gmail.com>
2026-02-25 17:04:50 +0530

    Order Tracking page

---
+++ Client/Cravora-Client/src/pages/OrderTracking.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { useParams, Link } from 'react-router-dom';
+import { ArrowLeft } from 'lucide-react';
+import Navbar from '../components/Navbar';
+import OrderTracker from '../components/OrderTracker';
+import './StudentDashboard.css';
+
+const OrderTracking = () => {
+    const { id } = useParams();
+
+    return (
+        <div className="sd-page">
+            <Navbar />
+            <div className="sd-container">
+                <Link to="/student-dashboard" className="sd-back-link">
+                    <ArrowLeft size={16} /> Back to dashboard
+                </Link>
+
+                <OrderTracker orderId={id} />
+            </div>
+        </div>
+    );
+};
+
+export default OrderTracking;
